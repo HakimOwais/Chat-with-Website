7db39e829be7a76e5777d1d37f4748fa851713a7
alquantumai <owaisibnmushtaq@gmail.com>
2024-02-21 11:12:05 +0530

    chatbox and url box created

--- app.py
+++ app.py
@@ -11,3 +11,15 @@ st.title("Chat with your Website")
 with st.sidebar:
     st.header("Menu")
     website_url = st.text_input("Enter Your URL")
+    
+st.chat_input("Type you message here...")
+
+with st.chat_message("AI Chatbot"):
+    st.write("Hello, how can I help you")
+    
+with st.chat_message("Human"):
+    st.write("I want to know about LangChain")
+    
+with st.chat_message("AI Chatbot"):
+    st.write("Why")
+    
